230b0145af3f81eb79d2931ef7a6a0c61b2b7cf6
jose oliveira <74796059+Mikezpo@users.noreply.github.com>
2023-05-31 18:29:37 +0100

    Updated style.css

--- js/main.js
+++ js/main.js
@@ -5,3 +5,22 @@ const clearInput = () => {
   
   const clearBtn = document.getElementById('clearButton');
   clearBtn.addEventListener('click', clearInput);
+
+// NAVBAR SLIDER
+
+const toggleButton = document.getElementsByClassName('navButton')[0];
+const navbarLinks = document.getElementsByClassName('navLinks')[0];
+
+// Add click event listener to each menu item
+const menuItems = document.querySelectorAll('.menu-list a');
+for (const item of menuItems) {
+  item.addEventListener('click', () => {
+    // Close the menu when an item is clicked
+    navbarLinks.classList.remove('active');
+    toggle.checked = false;
+  });
+}
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active');
+});
