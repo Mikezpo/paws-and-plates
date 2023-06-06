81ad105003b7dfe1aaaa6199c23cea5840b5325b
jose oliveira <74796059+Mikezpo@users.noreply.github.com>
2023-06-06 22:24:52 +0100

    Updated footer section

--- js/main.js
+++ js/main.js
@@ -25,3 +25,19 @@ toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active');
 });
 
+// Footer liquid
+
+const wrapper = document.getElementById("bubble-wrapper");
+
+const animateBubble = x => {  
+  const bubble = document.createElement("div");
+  
+  bubble.className = "bubble";
+  bubble.style.left = `${x}px`;
+  
+  wrapper.appendChild(bubble);
+  
+  setTimeout(() => wrapper.removeChild(bubble), 2000);
+}
+
+window.onmousemove = e => animateBubble(e.clientX);
